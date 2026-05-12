7dc9426a0b566e3c72ca3ac9a71f81636af900e2
Eric Mignardi <mignardi.e@gmail.com>
2026-05-12 06:59:17 -0400

    Removed redundant tabs folder group

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,13 +1,55 @@
 import { useAuth } from "@/hooks/useAuth";
-import { Redirect, Stack } from "expo-router";
-import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Redirect, Tabs } from "expo-router";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 
 export default function AppLayout() {
-  const { session, loading } = useAuth();
+  const { signOut, session, loading } = useAuth();
+  const [signingOut, setSigningOut] = useState<boolean>(false);
+
+  const onSignOut = async () => {
+    setSigningOut(true);
+    const { error } = await signOut();
+    setSigningOut(false);
+    if (error) Alert.alert("Sign out failed", error.message);
+  };
 
   if (loading) return null;
 
   if (!session) return <Redirect href={"/(auth)/sign-up"} />;
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        // headerRight: () => (
+        //   <Pressable
+        //     disabled={signingOut}
+        //     onPress={onSignOut}
+        //     style={{ marginRight: 16 }}
+        //   >
+        //     <Text>{signingOut ? "..." : "Sign out"}</Text>
+        //   </Pressable>
+        // ),
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#000",
+        tabBarLabelStyle: { color: "#000" },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
